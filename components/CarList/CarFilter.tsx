0204a5ebddd2c1b990d4d5e2ec4e1163ac45171f
AdamSdes <111901788+AdamSdes@users.noreply.github.com>
2024-11-18 22:37:06 +0300

    add button

--- components/CarList/CarFilter.tsx
+++ components/CarList/CarFilter.tsx
@@ -1,6 +1,4 @@
-import {Image} from "@nextui-org/image";
 import {Button} from "@nextui-org/button";
-import {Chip} from "@nextui-org/chip";
 import {Slider} from "@nextui-org/slider";
 import {CheckboxGroup, Checkbox} from "@nextui-org/checkbox";
 import {Input} from "@nextui-org/input";
@@ -15,14 +13,14 @@ export const CarFilter = () => {
                     <p>Коробка передач</p>
                 </div>
                 <div className="flex flex-wrap gap-2">
-                <CheckboxGroup
-      label="Оберіть коробку передач"
-      defaultValue={["3"]}
-    >
-      <Checkbox value="Автоматична">Автоматична</Checkbox>
-      <Checkbox value="Механічна">Механічна</Checkbox>
-      <Checkbox value="3">Неважливо</Checkbox>
-    </CheckboxGroup>
+                    <CheckboxGroup
+                        label="Оберіть коробку передач"
+                        defaultValue={["3"]}
+                    >
+                        <Checkbox value="Автоматична">Автоматична</Checkbox>
+                        <Checkbox value="Механічна">Механічна</Checkbox>
+                        <Checkbox value="3">Неважливо</Checkbox>
+                    </CheckboxGroup>
                 </div>
             </div>
             <div className="p-5 bg-white/5 flex flex-col gap-5 rounded-[14px]">
@@ -30,15 +28,15 @@ export const CarFilter = () => {
                     <p>Привід</p>
                 </div>
                 <div className="flex flex-wrap gap-2">
-                <CheckboxGroup
-      label="Оберіть Привід"
-      defaultValue={["3"]}
-    >
-      <Checkbox value="Автоматична">Передній-Привід</Checkbox>
-      <Checkbox value="Механічна">Задній-Привід</Checkbox>
-      <Checkbox value="2">Повний-Привід</Checkbox>
-      <Checkbox value="3">Неважливо</Checkbox>
-    </CheckboxGroup>
+                    <CheckboxGroup
+                        label="Оберіть Привід"
+                        defaultValue={["3"]}
+                    >
+                        <Checkbox value="Автоматична">Передній-Привід</Checkbox>
+                        <Checkbox value="Механічна">Задній-Привід</Checkbox>
+                        <Checkbox value="2">Повний-Привід</Checkbox>
+                        <Checkbox value="3">Неважливо</Checkbox>
+                    </CheckboxGroup>
                 </div>
             </div>
             <div className="p-5 bg-white/5 flex flex-col gap-5 rounded-[14px]">
@@ -46,16 +44,16 @@ export const CarFilter = () => {
                     <p>Тип Кузову</p>
                 </div>
                 <div className="flex flex-wrap gap-2">
-                <CheckboxGroup
-      label="Оберіть тип кузову"
-      defaultValue={["3"]}
-    >
-      <Checkbox value="Седан">Седан</Checkbox>
-      <Checkbox value="Механічна">Купе</Checkbox>
-      <Checkbox value="2">Універсал</Checkbox>
-      <Checkbox value="1">Хетчбек</Checkbox>
-      <Checkbox value="3">Неважливо</Checkbox>
-    </CheckboxGroup>
+                    <CheckboxGroup
+                        label="Оберіть тип кузову"
+                        defaultValue={["3"]}
+                    >
+                        <Checkbox value="Седан">Седан</Checkbox>
+                        <Checkbox value="Механічна">Купе</Checkbox>
+                        <Checkbox value="2">Універсал</Checkbox>
+                        <Checkbox value="1">Хетчбек</Checkbox>
+                        <Checkbox value="3">Неважливо</Checkbox>
+                    </CheckboxGroup>
                 </div>
             </div>
             <div className="p-5 bg-white/5 flex flex-col gap-5 rounded-[14px]">
@@ -63,15 +61,15 @@ export const CarFilter = () => {
                     <p>Ціна</p>
                 </div>
                 <div className="flex flex-wrap gap-2">
-                <Slider 
-                    label="."
-                    step={50} 
-                    minValue={0} 
-                    maxValue={200000} 
-                    defaultValue={[100, 500]} 
-                    formatOptions={{style: "currency", currency: "USD"}}
-                    className="max-w-md"
-    />
+                    <Slider
+                        label="."
+                        step={50}
+                        minValue={0}
+                        maxValue={200000}
+                        defaultValue={[100, 100000]}
+                        formatOptions={{style: "currency", currency: "USD"}}
+                        className="max-w-md"
+                    />
                 </div>
             </div>
             <div className="p-5 bg-white/5 flex flex-col gap-5 rounded-[14px]">
@@ -79,19 +77,23 @@ export const CarFilter = () => {
                     <p>Рік Випуску</p>
                 </div>
                 <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-      <Input type="Рік" label="Від" />
-      <Input type="Рік" label="До" />
-    </div>
+                    <Input type="Рік" label="Від"/>
+                    <Input type="Рік" label="До"/>
+                </div>
             </div>
             <div className="p-5 bg-white/5 flex flex-col gap-5 rounded-[14px]">
                 <div className="">
                     <p>Літраж мотору</p>
                 </div>
                 <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-      <Input type="Літрів" label="Від" />
-      <Input type="Літрів" label="До" />
-    </div>
-    </div>
+                    <Input type="Літрів" label="Від"/>
+                    <Input type="Літрів" label="До"/>
+                </div>
+            </div>
+            <div className="flex gap-5 items-center">
+                <Button color='default' className='w-full py-8 max-h-[60px]'>Очистити</Button>
+                <Button color='primary' className='w-full py-8 max-h-[60px]'>Застосувати</Button>
+            </div>
         </main>
     )
 
