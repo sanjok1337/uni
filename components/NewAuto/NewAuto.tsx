cb7f5ac9ed618eb0b6d450cfc4e0517bb8ad1d14
sanjok1337 <sashametel@gmail.com>
2024-12-09 20:10:31 +0200

    Initial commit3

--- components/NewAuto/NewAuto.tsx
+++ components/NewAuto/NewAuto.tsx
@@ -1,16 +1,24 @@
-import {Button} from "@nextui-org/button";
-import {Image} from "@nextui-org/image";
+"use client";
 
+import { Button } from "@nextui-org/button";
+import { useRouter } from "next/navigation"; // Next.js router
 
 export const NewAuto = () => {
+    const router = useRouter(); // Ініціалізація useRouter
 
     return (
         <main className="container mx-auto max-w-[1550px] py-20 px-6">
             <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 items-center">
                 <div className="flex flex-col gap-5">
                     <h1 className="text-[36px] font-black">Нові автомобілі у наявності</h1>
-                    <p className="text-[14px] w-[70%] opacity-60">Пориньте у світ Audi прямо зараз із нашими новими автомобілями, що вже є у наявності! Це дуже зручно — одразу сісти у свій новий автомобіль і поїхати. Наші нові автомобілі оснащені інноваційними технологіями та функціями, які забезпечать неповторні враження від водіння. Відкрийте для себе ідеальне поєднання динамічності, елегантності та комфорту.</p>
-                    <Button className="w-fit">Перейти</Button>
+                    <p className="text-[14px] w-[70%] opacity-60">
+                        Пориньте у світ Audi прямо зараз із нашими новими автомобілями, що вже є у наявності! Це дуже зручно — одразу сісти у свій новий автомобіль і поїхати. 
+                        Наші нові автомобілі оснащені інноваційними технологіями та функціями, які забезпечать неповторні враження від водіння. 
+                        Відкрийте для себе ідеальне поєднання динамічності, елегантності та комфорту.
+                    </p>
+                    <Button className="w-fit" onClick={() => router.push("/new-cars")}>
+                        Перейти
+                    </Button>
                 </div>
                 <div className="">
                     <img
@@ -21,8 +29,7 @@ export const NewAuto = () => {
                 </div>
             </div>
         </main>
-    )
+    );
+};
 
-}
-
-export default NewAuto
+export default NewAuto;
